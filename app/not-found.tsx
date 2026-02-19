7c256520556c9fb12e20484ae0da9d8378f7145c
Kristijan Jovanovski <kristijan.j92@gmail.com>
2026-02-19 20:08:04 +0100

    custom not found

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h2 className="text-3xl font-semibold mb-4">Not Found</h2>
+        <p className="mb-8">Could not find the requested resource.</p>
+        <Link
+          href="/"
+          className="bg-primary py-4 px-8 text-white hover:bg-primary/90 transition"
+        >
+          Return Home
+        </Link>
+      </div>
+    </section>
+  );
+}
